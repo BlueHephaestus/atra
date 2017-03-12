2b9d9babcd9dbecc76542c73fc482dcf45d46a26
DarkElement75 <blakeedwards777@gmail.com>
2017-03-12 16:46:23 -0600

    Added display of capsule contributions, and added visual entity probabilities

--- atra1_2.py
+++ atra1_2.py
@@ -15,7 +15,7 @@ Features Added
 """
 import sys
 import numpy as np
-np.random.seed(420)#For ease of testing
+#np.random.seed(420)#For ease of testing
 
 import atra_base
 from atra_base import *
@@ -63,11 +63,18 @@ Define inputs,
         Of course, we can easily manipulate the input dimensions to have (784,) or (28,28) or (784, 1, 1) or (28, 28, 1), and so on.
 """
 
-capsule_n = 3
+capsule_n = 20
+epochs = 50
+mini_batch_size= 256
 dropout_p = 0.0
 regularization_rate = 0.00
+activation_fn = "relu"
+loss = "binary_crossentropy"
+optimizer = "adadelta"
 encoder_output_dim = [3,3]
 input_dims = [28,28]
+
+graph_output = False
 
 flattened_output_dims = np.prod(input_dims)
 flattened_encoder_output_dim = np.prod(encoder_output_dim)
@@ -112,7 +119,7 @@ We can represent our model by first copying our input into each capsule by creat
     Then finally merging the layers when we get our output at the end.
 """
 for capsule_i in range(capsule_n):
-    capsule_outputs.append(Dense(flattened_encoder_output_dim, activation="relu", W_regularizer=l2(regularization_rate))(flattened_inputs))
+    capsule_outputs.append(Dense(flattened_encoder_output_dim, activation=activation_fn, W_regularizer=l2(regularization_rate), name="capsule_%i_recognition_units1"%(capsule_i))(flattened_inputs))
     capsule_outputs[capsule_i] = Dropout(dropout_p)(capsule_outputs[capsule_i])
 
 """
@@ -124,16 +131,28 @@ Invariant Visual Entity Probabilities
 """
 capsule_entity_probabilities = []
 for capsule_i in range(capsule_n):
-    capsule_entity_probabilities.append(Dense(1, activation="relu", W_regularizer=l2(regularization_rate))(capsule_outputs[capsule_i]))
+    capsule_entity_probabilities.append(Dense(1, activation=activation_fn, W_regularizer=l2(regularization_rate), name="capsule_%i_entity_probabilities"%(capsule_i))(capsule_outputs[capsule_i]))
 
 """
 BEGIN DECODER
 Currently, just one layer of generation units
 """
 for capsule_i in range(capsule_n):
-    capsule_outputs[capsule_i] = Dense(flattened_output_dims, activation="relu", W_regularizer=l2(regularization_rate))(capsule_outputs[capsule_i])
-    capsule_outputs[capsule_i] = Dropout(dropout_p)(capsule_outputs[capsule_i])
+    capsule_outputs[capsule_i] = Dense(flattened_output_dims, activation=activation_fn, W_regularizer=l2(regularization_rate))(capsule_outputs[capsule_i])
+    capsule_outputs[capsule_i] = Dropout(dropout_p, name="capsule_%i_generative_unit_outputs"%(capsule_i))(capsule_outputs[capsule_i])
 
+"""
+We then apply our invariant visual entity probabilities to each capsule's outputs, before getting the final summed output.
+    Since we are combining the outputs of two layers into one output (and not models, otherwise we would use Merge(capitalized)), we are doing this with a merge layer.
+    Since this merge is our own special operation, a scalar multiplication, we use lambda for it.
+        We do this simply with x[0] * x[1], since it's just cA, if c is a scalar and A is a matrix.
+        The output shape is "not including the batch size" according to the Keras documentation, so we get the int_shape of our original capsule output,
+            then get all but the first dimension, via [1:].
+    And that's it!
+    I would use K.shape, but the tensors it returns are not iterable, and this needs an iterable argument passed.
+"""
+for capsule_i in range(capsule_n):
+    capsule_outputs[capsule_i] = merge([capsule_entity_probabilities[capsule_i], capsule_outputs[capsule_i]], mode=lambda x: x[0]*x[1], output_shape=K.int_shape(capsule_outputs[capsule_i])[1:], name="capsule_%i_gating_unit_outputs"%(capsule_i))
 
 """
 With the atomic capsule model complete, we merge all our outputs together to get the atomic_capsule_outputs, initially with concatenation.
@@ -143,7 +162,7 @@ atomic_capsule_outputs = merge([capsule_output for capsule_output in capsule_out
 """
 We then reshape so as to get the image outputs, of the same shape as our labels / inputs
 """
-atomic_capsule_outputs = Reshape(capsule_output_dims)(atomic_capsule_outputs)
+atomic_capsule_outputs = Reshape(capsule_output_dims, name="atomic_capsule_outputs")(atomic_capsule_outputs)
 
 """
 Note: The dimensions / axes are (Mini batches, Capsules, ...)
@@ -160,19 +179,22 @@ With our inputs and outputs, create a Keras Model.
 model = Model(input=inputs, output=composite_capsule_output)
 
 """
-Initialize a session and get our outputs with some random inputs of shape (batch, input_dims[0], input_dims[1])
-sess = K.get_session()
-sample_inputs = np.ones((1,input_dims[0], input_dims[1], input_dims[2]))
-print sample_inputs
-a = sess.run(composite_capsule_output, feed_dict={inputs: sample_inputs})
-print a
-print a.shape
-"""
-"""
 Train the model and get results
 """
-model.compile(loss="categorical_crossentropy", optimizer="adadelta", metrics=["accuracy"])
-results = model.fit(X_train, X_train, nb_epoch=20, batch_size=50, shuffle=True)
+model.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
+results = model.fit(X_train, X_train, nb_epoch=epochs, batch_size=mini_batch_size, shuffle=True)
+
+"""
+Get individual capsule model so we can see outputs of individual capsules
+"""
+individual_capsule_model = Model(input=model.input, output=model.get_layer("atomic_capsule_outputs").output)
+
+"""
+Get individual capsule visual entity probability models so we can see probabilities between samples
+"""
+individual_capsule_entity_probability_models = []
+for capsule_i in range(capsule_n):
+    individual_capsule_entity_probability_models.append(Model(input=model.input, output=model.get_layer("capsule_%i_entity_probabilities"%(capsule_i)).output))
 
 """
 Get sample inputs and outputs
@@ -180,20 +202,62 @@ Get sample inputs and outputs
 display_test_n = 16
 sample_inputs = X_test[:display_test_n]
 sample_outputs = model.predict(sample_inputs)
+sample_individual_capsule_outputs = individual_capsule_model.predict(sample_inputs)
 
 """
-Concatenate each into their own row
+In order to get our individual capsule visual entity probability outputs
+    we first loop through and get predictions for each model, our scalars for each capsule
+This will result in a list of shape (capsule_n, display_test_n, 1), 
+    which we then convert to a np array and remove the extra dimension.
 """
-sample_inputs = get_concatenated_row(sample_inputs)
-sample_outputs = get_concatenated_row(sample_outputs)
+sample_individual_capsule_entity_probabilities = []
+for capsule_i in range(capsule_n):
+    sample_individual_capsule_entity_probabilities.append(individual_capsule_entity_probability_models[capsule_i].predict(sample_inputs))
+sample_individual_capsule_entity_probabilities = np.array(sample_individual_capsule_entity_probabilities)
+sample_individual_capsule_entity_probabilities = np.squeeze(sample_individual_capsule_entity_probabilities)
 
 """
-Get a comparison image by concatenating the rows on top of each other, then display
+Concatenate each into their own column
 """
-comparison_img = get_concatenated_col((sample_inputs, sample_outputs))
-disp_img_fullscreen(comparison_img)
+sample_inputs = get_concatenated_col(sample_inputs)
+sample_outputs = get_concatenated_col(sample_outputs)
+
+"""
+Get our individual capsule outputs in the following way:
+    Since sample_individual_capsule_outputs is of shape (display_test_n, capsule_n, h, w),
+        we loop through display_test_n for each row, and capsule_n for each column.
+"""
+disp_individual_capsule_outputs = []
+for sample_capsule_outputs in sample_individual_capsule_outputs:
+    """
+    Get the row of outputs for each sample, and append it
+    """
+    disp_individual_capsule_outputs.append(get_concatenated_row(sample_capsule_outputs))
+
+"""
+Then combine all our rows into a column, giving us all our outputs in the format we want.
+"""
+disp_individual_capsule_outputs = get_concatenated_col(disp_individual_capsule_outputs)
+
+
+"""
+Get a comparison image by concatenating the columns alongside each other, then display
+"""
+comparison_img = get_concatenated_row((sample_inputs, sample_outputs, disp_individual_capsule_outputs))
+
+"""
+Do 1-image so we get black on white background, instead of white on black
+"""
+#disp_img_fullscreen(comparison_img)
+disp_img_fullscreen(1-comparison_img)
+
+"""
+Print the visual entity probabilities for each capsule
+"""
+print sample_individual_capsule_entity_probabilities.transpose()
 
 """
 Graph our loss and accuracy
 """
-graph_results(results.history["loss"], results.history["acc"])
+if graph_output:
+    graph_results(results.history["loss"], results.history["acc"])
